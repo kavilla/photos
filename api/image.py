320ecb5b710b8d919d1feede6fffbf335d9703ac
Kawika Avilla <kavilla414@gmail.com>
2020-01-12 13:43:31 -0600

    Creating model.

--- api/image.py
+++ api/image.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restplus import Api, Resource, reqparse
 
 from init import df
-from models import NotFoundException
+from models import NotFoundException, ImageModel
 
 app = Flask(__name__)
 api = Api(app=app)
@@ -11,43 +11,49 @@ ns = api.namespace(
     description='Image operations'
 )
 
-parser = reqparse.RequestParser()
-parser.add_argument(
-    'page',
-    type=int,
-    default=0,
-    required=False,
-    help='Page Index'
-)
-parser.add_argument(
-    'count',
-    type=int,
-    default=10,
-    required=False,
-    help='Number of Results'
-)
 
-
-def map_to_url_list(data_list):
+def map_to_image_list(data_list):
     result = []
     for data in data_list:
-        result.append(f'{data[0]}//{data[1]}/{data[2]}/{data[3]}/{data[4]}/{data[5]}')
+        image_id = data[0]
+        width = data[1]
+        height = data[2]
+        result.append(ImageModel(image_id, width, height))
     return result
 
 
 @ns.route('')
 class ImageList(Resource):
+    def __init__(self, *args, **kwargs):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument(
+            'page',
+            type=int,
+            default=0,
+            required=False,
+            help='Page Index'
+        )
+        self.parser.add_argument(
+            'count',
+            type=int,
+            default=10,
+            required=False,
+            help='Number of Results'
+        )
+        super(ImageList, self).__init__(*args, **kwargs)
+
     @ns.doc(responses={
         200: 'Success'
     })
+    @ns.marshal_list_with(ImageModel.fields)
     def get(self):
-        args = parser.parse_args()
+        args = self.parser.parse_args()
         page = args['page']
         count = args['count']
         left = page * count
         right = left + count
 
-        return map_to_url_list(df[left:right].values.tolist())
+        return map_to_image_list(df[left:right].values.tolist())
 
 
 @ns.route('/<int:image_id>')
@@ -56,9 +62,10 @@ class Image(Resource):
         200: 'Success',
         404: 'Not Found'
     })
+    @ns.marshal_with(ImageModel.fields)
     def get(self, image_id):
         try:
-            result = map_to_url_list(df.query(f'id == {image_id}').values.tolist())
+            result = map_to_image_list(df.query(f'id == {image_id}').values.tolist())
 
             if len(result) == 0:
                 raise NotFoundException
--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
+from flask_restplus import fields
+
+from init import image_url
+
+
 # Custom Exception
 class NotFoundException(Exception):
     pass
+
+
+# Custom Classes
+class ImageModel:
+    fields = {
+        'image_id': fields.Integer,
+        'width': fields.Integer,
+        'height': fields.Integer,
+        'url': fields.String
+    }
+
+    def __init__(self, image_id, width, height):
+        self.image_id = image_id
+        self.width = width
+        self.height = height
+        self.url = image_url % {'image_id': image_id, 'width': width, 'height': height}
