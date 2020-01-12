2666d9987a71b008d2c9b3d365816878ddf0b4cc
Kawika Avilla <kavilla414@gmail.com>
2020-01-12 16:26:26 -0600

    Able to switch pages through UI.

--- app/src/components/Home/Home.js
+++ app/src/components/Home/Home.js
@@ -10,29 +10,70 @@ export default class Home extends React.Component {
   constructor(props) {
     super(props);
 
+    this.currentPageIndex = 0;
+
     this.state = {
+      isLoading: true,
       images: [],
     };
 
     ImageService.getImages().then(resp => {
       this.setState(() => ({
+        isLoading: false,
         images: resp,
       }));
     });
   }
 
+  handleUpdatePageIndex = value => {
+    const nextPageIndex = this.currentPageIndex + value >= 0 ? this.currentPageIndex + value : 0;
+
+    if (this.currentPageIndex === nextPageIndex) {
+      return;
+    }
+
+    this.currentPageIndex = nextPageIndex;
+
+    this.setState(() => ({
+      isLoading: true,
+    }));
+
+    ImageService.getImages(this.currentPageIndex).then(resp => {
+      this.setState(() => ({
+        isLoading: false,
+        images: resp,
+      }));
+    });
+  };
+
   render() {
-    const imageCards = this.state.images.map(image => (
-      <div className="card">
-        <div className="card-middle">
-          <img src={image.url} alt={image.url} />
-        </div>
+    const imageCards = !this.state.isLoading
+      ? this.state.images.map(image => (
+          <div className="card">
+            <div className="card-middle">
+              <img src={image.url} alt={image.id} />
+            </div>
+          </div>
+        ))
+      : null;
+
+    const prevButton = (
+      <div className="prev-button-container btn-primary" onClick={() => this.handleUpdatePageIndex(-1)}>
+        <span>&larr;</span>
       </div>
-    ));
+    );
+
+    const nextButton = (
+      <div className="next-button-container btn-primary" onClick={() => this.handleUpdatePageIndex(1)}>
+        <span>&rarr;</span>
+      </div>
+    );
 
     return (
       <div className="home">
         <div className="card-container">{imageCards}</div>
+        <div>{prevButton}</div>
+        <div>{nextButton}</div>
       </div>
     );
   }
