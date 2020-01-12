92bffdd5c81fe9704792a4e5704d4008b1ed42bd
Kawika Avilla <kavilla414@gmail.com>
2020-01-11 23:47:38 -0600

    Initial start (#4)

---
+++ app/src/components/Home/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import './Home.css';
+import './../../App.css';
+import ImageService from '../../services/ImageService';
+import ImageModel from '../../models/Image';
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: [],
+    };
+
+    ImageService.getImages().then(resp => {
+      this.setState(() => ({
+        images: resp,
+      }));
+    });
+  }
+
+  render() {
+    const imageCards = this.state.images.map(image => (
+      <div className="card">
+        <div className="card-middle">
+          <img src={image} alt="Image" />
+        </div>
+      </div>
+    ));
+
+    return (
+      <div className="home">
+        <div className="card-container">{imageCards}</div>
+      </div>
+    );
+  }
+}
