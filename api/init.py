2ddfb9d387f0db1d92449d8041b139e3a7c20d1a
Kawika Avilla <kavilla414@gmail.com>
2020-01-11 18:00:56 -0600

    CSV is read.

---
+++ api/init.py
@@ -0,0 +1,18 @@
+import glob
+
+import pandas as pd
+from flask import Flask
+from flask_cors import CORS
+from flask_restplus import Api
+
+api = Api(
+    title='goTenna API',
+    version='1.0',
+    description='API for photo viewing.'
+)
+
+app = Flask(__name__)
+
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+df = pd.read_csv(glob.glob("./data/*.csv")[0])
